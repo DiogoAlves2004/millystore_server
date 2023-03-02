5af5c61c391e62b3e4d66d2ee294206b45618ee0
Didizin <alvessilva524@gmail.com>
2023-03-02 12:19:03 -0300

    0.1

--- api/index.js
+++ api/index.js
@@ -1,5 +1,37 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
-const app = require('./app')
-const server = http.createServer(app);
-server.listen(port);
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+const bodyPaser = require('body-parser')
+require('dotenv').config()
+
+
+const rotaProdutos = require('./routes/produtos')
+const rotaPedidos = require('./routes/pedidos')
+
+
+app.use(morgan('dev'))
+
+
+
+
+//rotas
+app.use('/produtos', rotaProdutos)
+app.use('/pedidos', rotaPedidos)
+
+
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+})
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500)
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    })
+})
+
+app.listen(4000)
